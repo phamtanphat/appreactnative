b5490e0ced13a9fb967ec0584d8457c2c34eb2fe
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-03-24 11:14:03 +0700

    Update Singer.js

--- components/Singer.js
+++ components/Singer.js
@@ -1,45 +1,43 @@
-import React, { Component } from "react";
-import { Text, View, FlatList , Image , RefreshControl} from "react-native";
+import React, { Component } from 'react'
+import { Text, View , FlatList , TouchableOpacity , Image } from 'react-native'
 import axios from 'axios';
-export default class Singer extends Component {
-    state = {
-        loading : false,
-        singers: []
-    };
-    componentWillMount(){
-        this.getMoreSinger();
-    }
 
-    getMoreSinger = async() =>{
-        this.setState({loading : true});
-        const {data} = await axios.get('https://servertest12.herokuapp.com/singer');
-        this.setState({singers : data.singers.concat(this.state.singers)});   
-        this.setState({loading : false});
+export default class Singer extends Component {
+    constructor(props){
+        super(props);
+        this.state ={singers : []}
+    }
+    componentWillMount = async() => {
+        try {
+            const response = await axios.get("https://servertest12.herokuapp.com/singer")
+            this.setState({singers : response.data.singers})
+            
+        } catch (error) {
+            alert(error.message)
+        }
+        
+        
     }
     render() {
         return (
-        <View style={{flex : 1 , justifyContent : 'center'}}>
+        <View style={{flex : 1 , justifyContent : 'center' , backgroundColor : 'white' , paddingTop : 20}}>
             <View style={{ alignItems : 'center' , padding : 20}}>
                 <Text style={{fontSize : 20  }}> Singers Component </Text>
             </View>
             <FlatList
-                refreshControl={
-                    <RefreshControl
-                        refreshing={this.state.loading}
-                        onRefresh={this.getMoreSinger}   
-                    />
-                }
                 data={this.state.singers}
                 keyExtractor={item => item.id}
                 renderItem={param => (
-                    <View style={{ marginTop : 20 , marginHorizontal : 10 , padding : 10, flexDirection : 'row' , backgroundColor : '#EEE9F6'}}>
+                    <View style={{ marginTop : 20 , marginHorizontal : 10 , padding : 10, flexDirection : 'row' , backgroundColor : 'orange'}}>
                         <View style={{ margin : 5}}  >
                             <Image
-                                style={{width : 100 , height : 100}} 
-                                source={{uri : param.item.avatar}}>
+                                source={{uri : param.item.avatar}}
+                                style={{width : 100 , height : 100}} >
                             </Image>
+                            
                         </View>
                         <View style={{ justifyContent : 'space-around' }}>
+                        
                             <Text style={{fontWeight : 'bold' , fontSize : 20}}>{param.item.name}</Text> 
                             <Text style={{fontWeight : 'normal' , fontSize : 15}}>{param.item.email}</Text> 
                         </View>
@@ -47,7 +45,6 @@ export default class Singer extends Component {
                     </View>
                 )}
             >
-
             </FlatList>
         </View>
         );
